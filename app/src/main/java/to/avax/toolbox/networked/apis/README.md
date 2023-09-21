140b59cf6488661bd0346b59a7b3cec107866807
avaxbuildr <avaxbuildr@crypto.bi>
2023-09-21 01:17:05 -0300

    Implementing info.peers client example

---
+++ app/src/main/java/to/avax/toolbox/networked/apis/README.md
@@ -0,0 +1,7 @@
+# to.avax.toolbox.networked.apis
+
+Programs the consume AvalancheJ `to.avax.avalanche.apis` APIs go here
+
+## Additional Documentation
+
+For additional documentation, questions, bug reports and general support visit [Crypto.bi](https://crypto.bi/forum/)
---
+++ app/src/main/java/to/avax/toolbox/networked/apis/info/Peers.java
@@ -0,0 +1,23 @@
+/*
+ *
+ * AVAX Toolbox - An Avalanche Toolbox
+ * Copyright (C) 2023 @REKTBuildr
+ *
+ *
+ * For more information, visit:
+ * https://crypto.bi
+ *
+ *
+ **/
+
+package to.avax.toolbox.networked.apis.info;
+
+import to.avax.avalanche.network.Constants;
+import to.avax.avalanche.network.NetworkConfig;
+
+public class Peers {
+    public static void main(String[] args) {
+        NetworkConfig netConfig = Constants.getTestConfig();
+
+    }
+}
