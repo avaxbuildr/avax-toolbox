a1680526af7234958fb189d357e4d8d125b92442
avaxbuildr <avaxbuildr@crypto.bi>
2023-09-21 23:22:47 -0300

    Implemented info.peers client example

--- app/src/main/java/to/avax/toolbox/networked/apis/info/Peers.java
+++ app/src/main/java/to/avax/toolbox/networked/apis/info/Peers.java
@@ -12,12 +12,18 @@
 
 package to.avax.toolbox.networked.apis.info;
 
+import to.avax.avalanche.Avalanche;
+import to.avax.avalanche.common.RequestResponseData;
 import to.avax.avalanche.network.Constants;
 import to.avax.avalanche.network.NetworkConfig;
+
+import java.util.Map;
 
 public class Peers {
     public static void main(String[] args) {
         NetworkConfig netConfig = Constants.getTestConfig();
-
+        Avalanche avalanche = new Avalanche(netConfig);
+        RequestResponseData rrd = avalanche.getInfoAPI().callMethod("info.getNodeVersion", Map.of());
+        System.out.println(rrd);
     }
 }
